11b5fe3ad830f053541fdfa89fbced700d0ebd45
KamyakChanna <kamyakchanna1@gmail.com>
2024-07-31 23:19:49 +0530

    Version2

--- tweetkar/urls.py
+++ tweetkar/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("tweets/",include('tweets.urls')),
     path("accounts/",include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- tweets/forms.py
+++ tweets/forms.py
@@ -6,8 +6,26 @@ class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo'] 
+        widgets = {
+            'text': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your tweet'}),
+            'photo': forms.ClearableFileInput(attrs={'class': 'form-control-file'}),
+        }
 class User_Reg_Form(UserCreationForm):
     email = forms.EmailField()
     class Meta:
         model = User
         fields = ('username','email','password1','password2')
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your username'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm your password'
+            }),
+        }
